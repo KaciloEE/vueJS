00ee4725357549b79e5652120073e08e4b93eba5
e.kacilo <e.kacilo@urancompany.com>
2017-10-23 17:10:39 +0300

    vue

--- qwunder/server/controllers/ApiController.js
+++ qwunder/server/controllers/ApiController.js
@@ -3,7 +3,7 @@
 module.exports = {
   show (req, res) {
     try {
-      const song = ['a', 'c', '1'] // await Song.findById(req.params.songId)
+      const song = [{id: 1, name: 'Widgets 1'}, {id: 2, name: 'Widgets 2'}] // await Song.findById(req.params.songId)
       res.send(song)
     } catch (err) {
       res.status(500).send({
--- qwunder/server/routes.js
+++ qwunder/server/routes.js
@@ -2,5 +2,8 @@ const ApiController = require('./controllers/ApiController')
 
 
 module.exports = (app) => {
-  app.get('/api', ApiController.show)
+  app.get('/api', ApiController.show)	
+  app.post('/api', (req, res) => {
+    console.log(req.body)	
+  })
 }
